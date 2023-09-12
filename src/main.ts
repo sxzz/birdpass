278312e87a535ebf664b7b6fcbebb6f4631c4d4d
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-12 22:22:10 +0800

    fix: wechat browser

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,8 @@ import './styles/main.css'
 import 'uno.css'
 
 createApp(App).mount('#app')
+
+if (navigator.userAgent.includes('MicroMessenger')) {
+  // eslint-disable-next-line no-alert
+  alert('请在系统浏览器中打开，不支持微信内置浏览器')
+}
