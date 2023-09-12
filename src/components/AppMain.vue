5f381a4fc43fbc11ffb89769607fa323ca30830c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-12 20:58:01 +0800

    feat: loading

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,26 +4,35 @@ import { type FileWithHandle } from 'browser-fs-access'
 const name = useLocalStorage('name', '')
 const avatar = ref<FileWithHandle>()
 const previewAvatar = ref<string>()
+const loading = ref(false)
 
-const disabled = computed(() => !name.value || !avatar.value)
+const disabled = computed(() => loading.value || !name.value || !avatar.value)
 
 async function mint() {
   if (disabled.value) return
 
-  const image = (await fileToDataUrl(avatar.value!)).slice(
-    'data:image/jpeg;base64,'.length
-  )
-  const res = await fetch('https://birdpass.qaq.wiki/sign', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: name.value,
-      avatar: image,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  }).then((r) => r.json())
+  loading.value = true
+  try {
+    const image = (await fileToDataUrl(avatar.value!)).slice(
+      'data:image/jpeg;base64,'.length
+    )
+    const res = await fetch('https://birdpass.qaq.wiki/sign', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: name.value,
+        avatar: image,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((r) => r.json())
 
-  const link = `https://birdpass.qaq.wiki/pass?passid=${res.passid}`
-  location.href = link
+    const link = `https://birdpass.qaq.wiki/pass?passid=${res.passid}`
+    location.href = link
+  } catch {
+    // eslint-disable-next-line no-alert
+    alert('Failed to mint ticket')
+  } finally {
+    loading.value = false
+  }
 }
 </script>
 
@@ -44,7 +53,19 @@ async function mint() {
     <div flex="~ col gap6" mb4 items-center>
       <ImageInput v-model="avatar" v-model:preview="previewAvatar" />
       <TextInput v-model="name" placeholder="What's your username?" />
-      <button class="text-lg btn" :disabled="disabled" @click="mint">
+      <button
+        flex
+        items-center
+        gap-1
+        text-lg
+        btn
+        :class="{ 'animate-pulse': loading }"
+        :disabled="disabled"
+        @click="mint"
+      >
+        <div v-if="loading" preserve-3d animate-spin>
+          <div i-ri:loader-2-line />
+        </div>
         Mint
       </button>
     </div>
